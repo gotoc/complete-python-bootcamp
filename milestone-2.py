9b68e22b74c753d1c3c21651c191892445cd835c
Pieter Konink <pieter@kon.ink>
2016-07-12 15:18:55 -0700

    Milestone Project 2 - finished all but some presentation elements and a bug with ending the game on a $0 balance

--- milestone-2.py
+++ milestone-2.py
@@ -4,6 +4,8 @@
 import random
 
 INPUT_TEXT = {  'line':"-------------------------",
+                'line2':"+++++++++++++++++++++++++",
+                'welcome':"Welcome, %s. This is Blackjack. It's a card game.", # player.name
                 'ask_name':"Hello, Player %s. Please enter your name: ", # num_players
                 'ace':"Would you like your Ace to count for 1 or 11 points? Enter 1 or 11: ",
                 'bet':"%s, please enter a bet between 1 and %s: ", # player.name, player.balance
@@ -11,7 +13,10 @@ INPUT_TEXT = {  'line':"-------------------------",
                 'over':"%s busts with %s points.", # player.name, player.points
                 'under':"%s stood with %s points.", # player.name, player.points
                 'blackjack':"%s reached %s points.", # player.name, player.points
-                'replay':"Do you want to play again? (y or n): "
+                'balance':"%s's balance: $%s", # player.name, player.balance
+                'zero_balance':"Unfortunately, you have a balance of $0, so the game must end.",
+                'replay':"Do you want to play again? (y or n): ",
+                'goodbye':"Goodbye ... foolish human!"
                 }
 CARD_TEXT = {   'H':"Hearts",'D':"Diamonds",'S':"Spades",'C':"Clubs",
                 'J':"Jack",'Q':"Queen",'K':"King",'A':"Ace",
@@ -25,9 +30,11 @@ class Money(object):
         self.balance = balance
 
     def add_balance(self,amount):
+        '''Add amount to the balance.'''
         self.balance += amount
 
     def sub_balance(self,amount):
+        '''Subtract amount from the balance.'''
         self.balance -= amount
 
 
@@ -37,33 +44,23 @@ class Deck(object):
     rank = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
 
     def __init__(self):
-        # Build the deck of cards as list of tuples
+        '''Build the deck of cards as list of tuples and shuffle.'''
         self.deck = []
         for s in self.suit:
             for r in self.rank:
                 self.deck.append((s,r))
+        random.shuffle(self.deck)
 
     def reshuffle(self):
+        '''Reset the cards in the deck.'''
         self.__init__()
 
-    def get_card(self):
-        # grab random card from deck
-        while True:
-            c = random.randrange(0,51)
-            try:
-                return self.deck[c]
-                break
-            except IndexError:
-                continue
-
     def deal_card(self,amount=1):
-        # add to hand and delete from deck
+        '''Gets amount of random cards from deck and delete them there. Return list of cards.'''
         cards = []
         i = 1
         while i <= amount:
-            card = self.get_card()
-            cards.append(card)
-            self.deck.remove(card)
+            cards.append(self.deck.pop(0))
             i += 1
         return cards
 
@@ -75,7 +72,7 @@ class Points(object):
         self.dealer = dealer
 
     def set_points(self,cards,auto_ace=False):
-        # expect cards to be a list of tuples
+        '''Adds points to total based on cards inputted. auto_ace is a simple AI for selecting value of Ace.'''
         for c in cards:
             s,r = c
             try:
@@ -117,6 +114,7 @@ class Player(Money, Points):
         self.stand = False
 
     def add_card(self,cards,auto_ace=False):
+        '''Add an amount of cards to the player's hand.'''
         for c in cards:
             self.hand.append(c)
         self.set_points(cards,auto_ace)
@@ -129,6 +127,7 @@ class Player(Money, Points):
             self.stand = True
 
     def clear(self):
+        '''Reset all non-persistent values for the player.'''
         del self.hand[:]
         self.bet = 0
         self.points = 0
@@ -139,7 +138,8 @@ class Player(Money, Points):
 class Bet(object):
 
     def place_bet(self,player):
-        if player.dealer == False:
+        '''Asks for bet amount and sets player objects bet total.'''
+        if player.dealer == False and player.balance > 0:
             while True:
                 try:
                     the_bet = int(raw_input(INPUT_TEXT['bet']%(player.name,str(player.balance))))
@@ -148,8 +148,13 @@ class Bet(object):
                         break
                 except:
                     continue
+            return True
+        else:
+            print "You don't have enough money left. Sorry!"
+            return False
 
-    def settle_bet(self,player,condition='T'):
+    def settle_bet(self,player,condition):
+        '''Adds or subtracts bet from player object's balance and resets bet to 0.'''
         if condition == 'W':
             player.balance = player.bet + player.balance
         elif condition == "L":
@@ -160,6 +165,7 @@ class Bet(object):
 class Table(object):
 
     def draw_hand(self, player_obj):
+        '''Output the player's hand of cards.'''
         cards = []
         for c in player_obj.hand:
             s,r = c
@@ -171,12 +177,14 @@ class Table(object):
         print INPUT_TEXT['line']
 
     def draw_money(self, player_obj):
+        '''Output the player's balance and current bet.'''
         print INPUT_TEXT['line']
         print "%s's current bet: $%s"%(player_obj.name,str(player_obj.bet))
         print "%s's balance: $%s"%(player_obj.name,str(player_obj.balance))
         print INPUT_TEXT['line']
 
     def draw_turn_end(self,player_obj):
+        '''Output the information for the end of a turn.'''
         if player_obj.state == "over":
             print "* %s busted! *"%(player_obj.name)
         elif player_obj.state == "blackjack":
@@ -186,76 +194,43 @@ class Table(object):
         print INPUT_TEXT['line']
 
     def draw_round_end(self,players):
+        '''Output the information for the end of a round.'''
         # End the round
-        print "++++*****************++++\n       ROUND ENDED\n++++*****************++++"
+        print INPUT_TEXT['line2']
+        print "     ROUND OVER!"
+        print INPUT_TEXT['line2']
+        print INPUT_TEXT['line']
         for p in players:
             print INPUT_TEXT[p.state]%(p.name,p.points)
-        print "+++++++++++++++++++++++++"
+        print INPUT_TEXT['line']
         for p in players:
             if p.dealer == False:
-                print "%s's balance: $%s"%(p.name,str(p.balance))
+                print INPUT_TEXT['balance']%(p.name,str(p.balance))
+        print INPUT_TEXT['line2']
 
+class Game(object):
 
-class Turn(object):
-
+    deck = Deck()
+    bets = Bet()
     t = Table()
+    balance_alert = False
 
-    def __init__(self,deck,player):
-        self.player = player
-        # add condition for double(double bet, take card, stand)
-        # add condition for surrender(lose 1/2 bet and forfeit hand)
-
-        # Manual turn for human players
-        if self.player.dealer == False:
-            self.t.draw_hand(self.player)
-            while True:
-                if self.player.stand == False:
-                    response = raw_input(INPUT_TEXT['hit']%(self.player.name)).upper()
-                    if response == "Y":
-                        self.player.add_card(deck.deal_card(1))
-                        self.t.draw_hand(self.player)
-                    elif response == "N":
-                        self.player.stand = True
-                        break
-                else:
-                    break
-        # Automatic turn for robot dealer
-        elif self.player.dealer == True:
-            while self.player.points < 17:
-                self.player.add_card(deck.deal_card(1))
-            else:
-                self.player.stand = True
-            self.t.draw_hand(self.player)
-
-
-class Main():
-
-    # Player objects are stored in list
-    player = [Player(0,"Dealer",True)]
-
-    def __init__(self):
-        self.deck = Deck()
-        self.bets = Bet()
-        self.t = Table()
-        # Set up the players
-        self.num_players = 1  # can come back later to add input for multiplayer
-        i = 0
-        while i < self.num_players:
-            name = raw_input(INPUT_TEXT['ask_name']%(i))
-            self.player.append(Player(50,name))
-            i += 1
-        # Move dealer to the end of the list
-        self.player += [self.player.pop(0)]
+    def __init__(self,players):
+        self.player = players
         self.round()
-        self.replay()
+        if self.balance_alert == False:
+            self.replay()
+        else:
+            self.exit_game()
 
     def round(self):
+        '''A sequence of player turns.'''
         # Start the game
         for p in self.player:
             if p.dealer == True:
                 p.add_card(self.deck.deal_card(2))
             else:
-                print "Welcome, %s. This is Blackjack. It's a card game." %(p.name)
+                print INPUT_TEXT['welcome']%(p.name)
                 # Place bets before rounds begin
                 self.bets.place_bet(p)
                 self.t.draw_money(p)
@@ -265,13 +240,51 @@ class Main():
         for p in self.player:
             # only bother with turn player qualifies
             if p.stand == False:
-                print p.name + "'s turn..."
                 # Perform a player's turn in the game
-                Turn(self.deck,p)
+                self.turn(p)
                 self.t.draw_turn_end(p)
+        self.outcome()
         self.t.draw_round_end(self.player)
 
+    def outcome(self):
+        '''Determines the winner(s) and loser(s) of the round.'''
+        for p in self.player:
+            # dealer has no balance so not worth checking
+            if p.dealer == False:
+                # Player always loses on bust regardless of dealer hand.
+                if p.state == "over":
+                    self.bets.settle_bet(p,"L")
+                # Player wins unless dealer also has 21, in which case it's a tie.
+                elif p.state == "blackjack":
+                    if p.points > self.player[-1].points:
+                        self.bets.settle_bet(p,"W")
+                    elif p.points < self.player[-1].points and self.player[-1].points > 21:
+                        self.bets.settle_bet(p,"W")
+                    elif p.points == self.player[-1].points:
+                        self.bets.settle_bet(p,"T") #Tie
+                # If under 21, player wins when higher than dealer or dealer busts, loses when lower, and ties when the same.
+                elif p.state == "under":
+                    if p.points > self.player[-1].points:
+                        self.bets.settle_bet(p,"W")
+                    elif p.points < self.player[-1].points and self.player[-1].points > 21:
+                        self.bets.settle_bet(p,"W")
+                    elif p.points == self.player[-1].points:
+                        self.bets.settle_bet(p,"T") #Tie
+                    else:
+                        self.bets.settle_bet(p,"L")
+                # Prevent player with zero balance from playing without restarting
+                if p.balance == 0:
+                    self.balance_alert = True
+
+    def exit_game(self):
+        '''Exits the game.'''
+        if self.balance_alert == True:
+            print INPUT_TEXT['zero_balance']
+        else:
+            print INPUT_TEXT['goodbye']
+
     def replay(self):
+        '''Asks the player if they would like to play another round.'''
         # Give option to replay the game
         while True:
             again = raw_input(INPUT_TEXT['replay']).upper()
@@ -281,8 +294,54 @@ class Main():
                 self.deck.reshuffle()
                 self.round()
             else:
-                print "Goodbye ... foolish human!"
+                self.exit_game()
                 break
+
+    def turn(self,player):
+        '''Processes a specific player's turn within the game round.'''
+        # add condition for double(double bet, take card, stand)
+        # add condition for surrender(lose 1/2 bet and forfeit hand)
+        # Manual turn for human players
+        if player.dealer == False:
+            self.t.draw_hand(player)
+            while True:
+                if player.stand == False:
+                    response = raw_input(INPUT_TEXT['hit']%(player.name)).upper()
+                    if response == "Y":
+                        player.add_card(self.deck.deal_card(1))
+                        self.t.draw_hand(player)
+                    elif response == "N":
+                        player.stand = True
+                        break
+                else:
+                    break
+        # Automatic turn for robot dealer
+        elif player.dealer == True:
+            while player.points < 17:
+                player.add_card(self.deck.deal_card(1))
+            else:
+                player.stand = True
+            self.t.draw_hand(player)
+
+
+class Main():
+    # Player objects are stored in list, add dealer.
+    players = [Player(0,"Dealer",True)]
+
+    def __init__(self):
+        '''The main class for initializing the game.'''
+        # Set up the players
+        self.num_players = 1  # can come back later to add input for multiplayer
+        i = 0
+        while i < self.num_players:
+            name = raw_input(INPUT_TEXT['ask_name']%(i+1))
+            # Create a new player object and add to the list.
+            self.players.append(Player(50,name))
+            i += 1
+        # Move dealer to the end of the list
+        self.players += [self.players.pop(0)]
+        # Start the game.
+        Game(self.players)
 
 
 Main()
